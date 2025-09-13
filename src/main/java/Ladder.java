33cdf0ffd9c27d8e79a1b68105f564719d5f7f28
jyun27 <kimjyun27@gmail.com>
2025-09-13 09:15:53 +0900

    feat: 1주차 미션 완료

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -3,6 +3,8 @@ public class Ladder {
     private final int[][] rows;
 
     public Ladder(int row, int numberOfPerson) {
+
         rows = new int[row][numberOfPerson];
+        System.out.println("미션 완료");
     }
 }
