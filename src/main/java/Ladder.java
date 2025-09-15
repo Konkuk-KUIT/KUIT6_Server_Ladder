fabf083a0f5708f9b04660044fa90f677401d6b3
wsmt0214 <wisemantaek@naver.com>
2025-09-15 14:06:44 +0900

    미션

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,100 @@
+import java.awt.image.ColorModel;
+
 public class Ladder {
 
     private final int[][] rows;
 
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
+    }
+
+    public void drawLine(Position from, Position to) {
+        allCheck(from, to);
+
+        int previousX = Math.min(from.getX(), to.getX());
+
+        rows[from.getY()][previousX] = 1;
+    }
+
+    private void allCheck(Position from, Position to) {
+        //높이가 같지 않은 지점을 서로 이으려고 할 때
+        checkSameLevel(from, to);
+
+        //거리가 1이 아닌 경우
+        checkLength(from, to);
+
+        //사다리의 경계 또는 경계 밖 이으려고 할 때
+        checkBoundary(from, to);
+
+        //중복 검증
+        checkDuplicate(from, to);
+    }
+
+    private void checkDuplicate(Position from, Position to) {
+        if (canMoveLeft(from) || canMoveRight(from)
+        || canMoveLeft(to) || canMoveRight(to))
+            throw new IllegalStateException("중복해서 사다리를 이을 수 없습니다.");
+    }
+
+    public int run(int column) {
+        //범위를 넘어서서 시작
+        checkStartRange(column);
+
+        int currentX = column;
+        int currentY = 0;
+
+        boolean isMoved = false; //옆으로 이동했는지
+        while (currentY != rows.length - 1) {
+            //왼쪽으로 이동 가능?
+            if (!isMoved && canMoveLeft(new Position(currentX, currentY))) {
+                currentX--;
+                isMoved = true;
+                continue;
+            }
+            //오른쪽으로 이동 가능?
+            if (!isMoved && canMoveRight(new Position(currentX, currentY))) {
+                currentX++;
+                isMoved = true;
+                continue;
+            }
+            isMoved = false;
+            currentY++;
+        }
+        return currentX;
+    }
+
+    private boolean canMoveRight(Position cur) {
+        return cur.getX() != rows[0].length - 1 && rows[cur.getY()][cur.getX()] == 1;
+    }
+
+    private boolean canMoveLeft(Position cur) {
+        return cur.getX() != 0 && rows[cur.getY()][cur.getX() - 1] == 1;
+    }
+
+
+    private void checkStartRange(int column) {
+        if (column < 0 || column > rows[0].length - 1)
+            throw new IllegalStateException("경계를 넘어서서 접근이 불가합니다.");
+    }
+
+    private void checkBoundary(Position from, Position to) {
+        if (from.getY() == 0 || from.getY() == (rows.length - 1))
+            throw new IllegalStateException("사다리의 경계는 이을 수 없습니다.");
+
+        if (from.getX() < 0 || from.getX() > rows[0].length - 1
+                || to.getX() < 0 || to.getX() > rows[0].length - 1
+                || from.getY() < 0 || from.getY() > rows.length - 1
+                || to.getY() < 0 || to.getY() > rows.length - 1)
+            throw new IllegalStateException("경계 밖은 이을 수 없습니다.");
+    }
+
+    private static void checkLength(Position from, Position to) {
+        if (Math.abs(from.getX() - to.getX()) != 1)
+            throw new IllegalStateException("사다리는 1개씩 그릴 수 있으며 동일한 지점을 이을 수 없습니다.");
+    }
+
+    private static void checkSameLevel(Position from, Position to) {
+        if (from.getY() != to.getY())
+            throw new IllegalStateException("높이가 다른 지점은 이을 수 없습니다.");
     }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,83 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 class LadderTest {
+
+    @Test
+    @DisplayName("3X4 성공적으로 사다리 타기")
+    public void test1() {
+        Ladder ladder = new Ladder(4, 3);
+        ladder.drawLine(new Position(0, 1), new Position(1, 1));
+        ladder.drawLine(new Position(1, 2), new Position(2, 2));
+
+        assertThat(ladder.run(0)).isEqualTo(2);
+        assertThat(ladder.run(1)).isEqualTo(0);
+        assertThat(ladder.run(2)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("4X5 성공적으로 사다리 타기")
+    public void test2() {
+        Ladder ladder = new Ladder(5, 4);
+        ladder.drawLine(new Position(0, 1), new Position(1, 1));
+        ladder.drawLine(new Position(2, 1), new Position(3, 1));
+        ladder.drawLine(new Position(1, 2), new Position(2, 2));
+        ladder.drawLine(new Position(1, 3), new Position(2, 3));
+
+        assertThat(ladder.run(0)).isEqualTo(1);
+        assertThat(ladder.run(1)).isEqualTo(0);
+        assertThat(ladder.run(2)).isEqualTo(3);
+        assertThat(ladder.run(3)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("경계를 넘어서 사다리를 탈 때")
+    public void test3() {
+        Ladder ladder = new Ladder(4, 3);
+
+        assertThatThrownBy(() -> ladder.run(-1))
+                .isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> ladder.run(3))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("사다리의 경계를 이을 때")
+    public void test4() {
+        Ladder ladder = new Ladder(5, 4);
+        assertThatThrownBy(() -> ladder.drawLine(new Position(0, 0), new Position(1, 0)))
+                .isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> ladder.drawLine(new Position(0, 4), new Position(1, 4)))
+                .isInstanceOf(IllegalStateException.class);
+
+    }
+
+    @Test
+    @DisplayName("경계 밖을 이을 때")
+    public void test5() {
+        Ladder ladder = new Ladder(5, 4);
+        assertThatThrownBy(() -> ladder.drawLine(new Position(-1, 1), new Position(0, 1)))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("사다리를 여러 개 한 번에 이을 때")
+    public void test6() {
+        Ladder ladder = new Ladder(5, 4);
+        assertThatThrownBy(() -> ladder.drawLine(new Position(0, 1), new Position(2, 1)))
+                .isInstanceOf(IllegalStateException.class);
+    }
+    @Test
+    @DisplayName("높이가 다른 지점을 이을 때")
+    public void test7() {
+        Ladder ladder = new Ladder(5, 4);
+        assertThatThrownBy(() -> ladder.drawLine(new Position(0, 1), new Position(2, 2)))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
 
 }
