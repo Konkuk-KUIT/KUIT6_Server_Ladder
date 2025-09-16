2dbf3808064f36af28c2efd269c3652436dc9bbe
bucheasal <leejunwoo9@gmail.com>
2025-09-16 19:21:57 +0900

    그냥구현

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -3,6 +3,49 @@ public class Ladder {
     private final int[][] rows;
 
     public Ladder(int row, int numberOfPerson) {
+        row--;
+        numberOfPerson--;
         rows = new int[row][numberOfPerson];
     }
+
+    //  같은 높이에 연속된 사다리 존재x
+    public void drawLine(int row, int col) {
+        if (col < 0 &&rows[row][col + 1] == 1) { //인덱스 예외 나오나?
+            throw new IllegalArgumentException("같은 높이에서 연속된 선 불가능");
+        }
+        if (col > rows[0].length && rows[row][col - 1] == 1) {
+            throw new IllegalArgumentException("같은 높이에서 연속된 선 불가능");
+        }
+        if (rows[row][col] == 1) {
+            throw new IllegalArgumentException("중복된 선");
+        }
+        rows[row][col] = 1;
+    }
+
+    public int run(int col) {
+        //진행 위치
+        int height = 0;
+        int colPosition = col-1; //0-base
+
+        while (height < rows.length) {
+            if (colPosition > 0 && rows[height][colPosition - 1] == 1) {
+                colPosition--;
+            }
+            if (colPosition + 1 < rows[0].length && rows[height][colPosition + 1] == 1) {
+                colPosition++;
+            }
+            height++;
+        }
+        return ++colPosition; //1-base
+    }
+
+//    public void printLadder() {
+//        for (int i = 0; i < rows.length; i++) {
+//            for (int j = 0; j < rows[0].length; j++) {
+//                System.out.print(rows[i][j]+"\t");
+//            }
+//            System.out.println();
+//        }
+//    }
+
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+    final int row = 10;
+    final int people = 3;
 
+    @Test
+    @DisplayName("빈 사다리 생성")
+    void 초기생성() {
+        Ladder ladder = new Ladder(row, people);
+        int position;
+        ladder.drawLine(0, 0);
+        ladder.drawLine(1, 1);
+        assertThat(ladder.run(1)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("중복 선 체크")
+    void 중복선() {
+        Ladder ladder = new Ladder(row, people);
+        int position;
+        ladder.drawLine(1, 1);
+        assertThatThrownBy(() -> ladder.drawLine(1, 1)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
