4ab92d0ff54b174e39acc2a70b2c72ce19e7b313
ParkSeongGeun <phd0801@naver.com>
2025-09-16 15:45:36 +0900

    [Fix] use InvalidLadderHeightException

--- src/main/java/wrap/LadderHeight.java
+++ src/main/java/wrap/LadderHeight.java
@@ -1,13 +1,14 @@
 package wrap;
 
 import exceptions.ErrorMessage;
+import exceptions.InvalidLadderHeightException;
 
 public class LadderHeight {
     private final int value;
 
     public LadderHeight(int value) {
         if (value <= 0) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_LADDER_HEIGHT.getMessage());
+            throw new InvalidLadderHeightException(ErrorMessage.INVALID_LADDER_HEIGHT.getMessage());
         }
         this.value = value;
     }
