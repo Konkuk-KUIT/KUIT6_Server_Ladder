de306535c3edf9ec52439faa5a3ff67bf78d99f8
yeonzeop <ksp2617@naver.com>
2025-09-17 10:30:19 +0900

    fix: 사다리 타기 간 간격 1로 고정

--- src/main/java/validator/SequenceValidator.java
+++ src/main/java/validator/SequenceValidator.java
@@ -4,7 +4,7 @@ import exception.IllegalSequenceException;
 
 public class SequenceValidator {
     public static void validateSequence(int left, int right) {
-        if (left >= right)
+        if (right - left != 1)
             throw new IllegalSequenceException();
     }
 }
