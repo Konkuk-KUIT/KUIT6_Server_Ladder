3efa3278a1d882dc8f50a113a36fe0ae4d063605
ParkSeongGeun <phd0801@naver.com>
2025-09-22 20:13:41 +0900

    AppConfig에서 Observer 자동 초기화 설정
    - AppConfig가 LadderGameFactory를 사용하도록 변경
    - setupObservers 메서드로 LadderPrinter 자동 등록
    - LadderGame에 getRunner 메서드 추가
    - Normal과 Random 사다리 생성 메서드 분리

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,23 +1,33 @@
-import ladderboard.board.Board;
-import ladderboard.LadderCreator;
-import runner.GameRunner;
+import game.LadderGame;
+import game.LadderGameFactory;
+import observer.LadderPrinter;
 import runner.LadderRunner;
 import wrap.PersonCount;
 import wrap.LadderHeight;
 
 public class AppConfig {
     
-    public Board createBoard(PersonCount personCount, LadderHeight ladderHeight) {
-        return new LadderCreator(personCount, ladderHeight);
-    }
-    
-    public GameRunner createRunner() {
-        return new LadderRunner();
-    }
-    
-    public LadderGame createLadder(int numberOfPerson, int height) {
+    public LadderGame createNormalLadder(int numberOfPerson, int height) {
         PersonCount personCount = PersonCount.of(numberOfPerson);
         LadderHeight ladderHeight = LadderHeight.of(height);
-        return new LadderGame(createBoard(personCount, ladderHeight), createRunner());
+        
+        LadderGame game = LadderGameFactory.createNormalLadderGame(personCount, ladderHeight);
+        setupObservers(game);
+        return game;
+    }
+
+    public LadderGame createRandomLadder(int numberOfPerson, int height) {
+        PersonCount personCount = PersonCount.of(numberOfPerson);
+        LadderHeight ladderHeight = LadderHeight.of(height);
+        
+        LadderGame game = LadderGameFactory.createRandomLadderGame(personCount, ladderHeight);
+        setupObservers(game);
+        return game;
+    }
+    
+    private void setupObservers(LadderGame game) {
+        LadderRunner runner = (LadderRunner) game.getRunner();
+        LadderPrinter printer = new LadderPrinter();
+        runner.addObserver(printer);
     }
 }
