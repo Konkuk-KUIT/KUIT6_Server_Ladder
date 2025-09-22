80756cb66af38c75001a2d529f3363655f450391
yeonzeop <ksp2617@naver.com>
2025-09-22 14:17:53 +0900

    test: 사다리 출력 기능

---
+++ src/test/java/LadderPrintTest.java
@@ -0,0 +1,18 @@
+import domain.LadderGame;
+import domain.Liner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LadderPrintTest {
+    @Test
+    @DisplayName("")
+    void print_simple_ladder(){
+        Liner liner = new Liner(5, 3);
+        liner.drawLine(1, 2, 1);
+        liner.drawLine(2, 3, 4);
+        LadderGame ladderGame = LadderGame.from(liner);
+
+        ladderGame.run(3);
+    }
+
+}
