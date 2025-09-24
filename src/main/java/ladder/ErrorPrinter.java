48a34f214fc32517b09d8678f0d1a975f60c5fdd
seungmo <boorue03@gmail.com>
2025-09-24 16:18:48 +0900

    피드백 적용해 코드 개선했습니다.

--- src/main/java/ladder/ErrorPrinter.java
+++ src/main/java/ladder/ErrorPrinter.java
@@ -1,8 +1,6 @@
 package ladder;
 
 
-import javax.lang.model.type.ErrorType;
-
 public class ErrorPrinter {
     public static void printError(ErrorMessage error) {
         System.out.println(error.toString());
