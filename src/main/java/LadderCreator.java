d2c81a794d4d49fcb0d06c8c1644759325650b29
hyungyu-02 <yona20020209@gmail.com>
2025-09-24 20:10:32 +0900

    feat: create LadderCreator and LadderCreatorTest

---
+++ src/main/java/LadderCreator.java
@@ -0,0 +1,37 @@
+public class LadderCreator {
+    private final Row[] rows;
+
+    public int getLadderInfoByPosition(Position position) {
+        validLadderBound(position);
+        return rows[position.getY()].getValue(position.getX());
+    }
+
+    public Row[] getRows() {
+        return rows;
+    }
+
+    public LadderCreator(GreaterThanOne numberOfRows, GreaterThanOne numberOfPerson) {
+        rows = new Row[numberOfPerson.getNumber()];
+        for (int i = 0; i < numberOfRows.getNumber(); i++) {
+            rows[i] = new Row(numberOfPerson);
+        }
+    }
+
+    public void drawLine(Position pos1, Position pos2) {
+        validDrawPosition(pos1, pos2);
+        rows[pos1.getY()].drawLine(pos1, pos2);
+    }
+
+    // valid methods
+    private void validLadderBound(Position position) {
+        if (position.getX() >= rows[0].getLength() || position.getY() >= rows.length) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_POSITION.getMessage());
+        }
+    }
+
+    private void validDrawPosition(Position pos1, Position pos2) {
+        if (pos1.getY() >= rows.length || pos2.getY() >= rows.length) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DRAW_POSITION.getMessage());
+        }
+    }
+}
---
+++ src/test/java/LadderCreatorTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LadderCreatorTest {
+    @Test
+    @DisplayName("초기 사다리는 0으로 초기화")
+    void initialLadder() {
+        // given
+        LadderCreator ladderCreator = new LadderCreator(GreaterThanOne.from(3), GreaterThanOne.from(3));
+
+        //when
+        Position pos = new Position(0, 0);
+
+        //then
+        assertThat(ladderCreator.getLadderInfoByPosition(pos)).isEqualTo(0);
+
+    }
+
+    @Test
+    @DisplayName("사다리 그리면 거리만큼 반영")
+    void drawComplete() {
+        // given
+        LadderCreator ladderCreator = new LadderCreator(GreaterThanOne.from(3), GreaterThanOne.from(3));
+        Position pos1 = new Position(0, 0);
+        Position pos2 = new Position(1, 0);
+
+        //when
+        ladderCreator.drawLine(pos1, pos2);
+
+        //then
+        assertThat(ladderCreator.getLadderInfoByPosition(pos1)).isEqualTo(1);
+        assertThat(ladderCreator.getLadderInfoByPosition(pos2)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("사다리 타기 정상 작동")
+    void drawLineAndRun() {
+        // given
+        LadderCreator ladderCreator = new LadderCreator(GreaterThanOne.from(5), GreaterThanOne.from(5));
+        LadderGame ladderGame = new LadderGame(ladderCreator);
+
+        // y=0: (0 <-> 1)
+        ladderCreator.drawLine(new Position(0, 0), new Position(1, 0));
+        // y=1: (2 <-> 3)
+        ladderCreator.drawLine(new Position(2, 1), new Position(3, 1));
+        // y=3: (1 <-> 2)
+        ladderCreator.drawLine(new Position(1, 3), new Position(2, 3));
+
+        // when & then
+        assertThat(ladderGame.run(0)).isEqualTo(2);
+        assertThat(ladderGame.run(1)).isEqualTo(0);
+        assertThat(ladderGame.run(2)).isEqualTo(3);
+        assertThat(ladderGame.run(3)).isEqualTo(1);
+        assertThat(ladderGame.run(4)).isEqualTo(4);
+
+    }
+}
