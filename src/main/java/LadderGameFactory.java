15d112707be6b017c261ab8457b5ec8586afff68
hyungyu-02 <yona20020209@gmail.com>
2025-09-24 21:20:35 +0900

    feat: create LadderGameFactory and use it in Main

---
+++ src/main/java/LadderGameFactory.java
@@ -0,0 +1,18 @@
+public class LadderGameFactory {
+    public static LadderCreator createNormalLadderGame(GreaterThanOne numberOfRows, GreaterThanOne numberOfPerson) {
+        return new LadderCreator(numberOfRows, numberOfPerson);
+    }
+
+    public static LadderCreator createRandomLadderGame(GreaterThanOne numberOfRows, GreaterThanOne numberOfPerson) {
+        LadderCreator ladderCreator = new LadderCreator(numberOfRows, numberOfPerson);
+
+        // y=0: (0 <-> 1)
+        ladderCreator.drawLine(new Position(0, 0), new Position(2, 0));
+        // y=1: (2 <-> 3)
+        ladderCreator.drawLine(new Position(2, 1), new Position(3, 1));
+        // y=3: (1 <-> 2)
+        ladderCreator.drawLine(new Position(1, 3), new Position(2, 3));
+
+        return ladderCreator;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,22 @@ public class Main {
         GreaterThanOne numberOfRows = GreaterThanOne.from(5);
         GreaterThanOne numberOfPlayers = GreaterThanOne.from(5);
 
-        LadderCreator ladderCreator = new LadderCreator(numberOfRows, numberOfPlayers);
-        // y=0: (0 <-> 1)
-        ladderCreator.drawLine(new Position(0, 0), new Position(2, 0));
-        // y=1: (2 <-> 3)
-        ladderCreator.drawLine(new Position(2, 1), new Position(3, 1));
-        // y=3: (1 <-> 2)
-        ladderCreator.drawLine(new Position(1, 3), new Position(2, 3));
+//        LadderCreator ladderCreator = new LadderCreator(numberOfRows, numberOfPlayers);
+//        // y=0: (0 <-> 1)
+//        ladderCreator.drawLine(new Position(0, 0), new Position(2, 0));
+//        // y=1: (2 <-> 3)
+//        ladderCreator.drawLine(new Position(2, 1), new Position(3, 1));
+//        // y=3: (1 <-> 2)
+//        ladderCreator.drawLine(new Position(1, 3), new Position(2, 3));
+//
+//
+//        LadderGame ladderGame = new LadderGame(ladderCreator);
+//        int finalPosition = ladderGame.run(0);
+//        System.out.println("\n---- finalPosition = " + finalPosition + " ----");
 
-
-        LadderGame ladderGame = new LadderGame(ladderCreator);
+        LadderCreator ladderCreator2 = LadderGameFactory.createRandomLadderGame(numberOfRows, numberOfPlayers);
+        LadderGame ladderGame = new LadderGame(ladderCreator2);
         int finalPosition = ladderGame.run(0);
-        System.out.println("\n---- finalPosition = " + finalPosition + " ----");
+
     }
 }
