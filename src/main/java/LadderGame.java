7a12c1b169f92540595127fab834b49c6f329e03
hyungyu-02 <yona20020209@gmail.com>
2025-09-24 20:10:46 +0900

    feat: create LadderGame and LadderGameTest

---
+++ src/main/java/LadderGame.java
@@ -0,0 +1,18 @@
+public class LadderGame {
+    private final LadderCreator ladderCreator;
+
+    public LadderGame(LadderCreator ladderCreator) {
+        this.ladderCreator = ladderCreator;
+    }
+
+    public int run(int startX) {
+        // 1. 사다리의 생성을 요청
+        Row[] rows = ladderCreator.getRows();
+
+        // 2. runner 에게 완성된 사다리를 건네준다.
+        LadderRunner ladderRunner = new LadderRunner(rows);
+
+        // 3. runner 에게 사다리를 타라고 메세지를 보낸다.
+        return ladderRunner.run(startX);
+    }
+}
